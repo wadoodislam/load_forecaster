a7188c50e5e16fde79de192f4def232535cb340f
WadoodAmer <wadood.islam@arbisoft.com>
2019-03-21 19:31:03 +0500

    Initial commit

---
+++ demo.py
@@ -0,0 +1,16 @@
+# from datetime import datetime as dt, timedelta
+import pandas as pd
+import load_forecast.loadForecast as lf
+
+f = 'data/NCENT.csv'
+df = pd.read_csv(f)
+all_X = lf.makeUsefulDf(df)
+all_y = df['load']
+predictions, accuracy = lf.neural_net_predictions(all_X, all_y)
+print('Percent accuracy (MAPE). Train: {}.  Test: {}'.format(100-accuracy['train'], 100-accuracy['test']))
+#
+# df_r = pd.DataFrame()
+# df_r['predicted_load'] = predictions
+# df_r['actual_load'] = [float(f) for f in all_y[-8760:]]
+# df_r.index = [dt(2018, 1, 1, 0) + timedelta(hours=1)*i for i in range(8760)]
+# df_r.plot(figsize=(10, 3), title="NCENT Texas 2018: prediction v. actual load")
